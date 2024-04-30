cdb2ab9632465d0659e11a3109d952dce68d2dc9
Myz <mylittleviruz@outlook.es>
2024-04-30 13:01:33 -0600

    Getting started with typing

--- 01-TypeScript-intro/src/main.ts
+++ 01-TypeScript-intro/src/main.ts
@@ -1,22 +1,22 @@
+import { name } from './bases/01-types'
+
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+
+
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
     <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
-    <h1>Vite + TypeScript</h1>
+    <h1>Vite + TypeScript ${ name }</h1>
     <div class="card">
-      <button id="counter" type="button"></button>
+      <button  type="button"></button>
     </div>
     <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
+      Click on the Vite and TypeScript logos to learn more!!!
+  
     </p>
   </div>
 `
